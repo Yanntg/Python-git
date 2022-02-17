2565ef84987abc355ab9acbfd8caaf12d86ed717
Yanntg <yanntgbr@gmail.com>
2022-02-17 18:05:13 -0300

    primeiro commit?

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+print("gyaaaa" + "2")
+numero = 3
+print("gyaaaa" + str(numero))
+
---
+++ Untitled-2.py
@@ -0,0 +1,13 @@
+print("aaaaa")
+friends = ["mike", "tyson"]
+print(friends)
+not_friends = ["bob"]
+friends.extend(not_friends)
+print(friends[1])
+
+def funcao(xingamento, numero):
+    print("Eae men kkj " + xingamento + " " + numero)
+
+xing = input("roast me daddy \n")
+numero_aleatorio = input()
+funcao(xing, numero_aleatorio)
