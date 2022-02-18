658d32624352f3af52770101535cdf1e6e35d385
Yanntg <yanntgbr@gmail.com>
2022-02-18 18:39:02 -0300

    Added PractPy3

---
+++ PractPy3.py
@@ -0,0 +1,2 @@
+n = int(input("Pick a number!\n"))  # Third extra
+print([i for i in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if i < n])  # exercise and extra 1 in one line
--- practicepy1.py
+++
@@ -1,2 +0,0 @@
-age = int(input("type in your age "))
-print("you will turn 100 years old on year " + str(2021 + (100-age)))
