1b70634a74859e3806b7e303478efddb1a3a250c
Yanntg <yanntgbr@gmail.com>
2022-03-21 13:47:36 -0300

    commented PractPy9

--- PractPy9.py
+++ PractPy9.py
@@ -1,21 +1,21 @@
 import random
 
 n = random.randint(1, 9)  # generating the random number
-guess = 0
-count = 0
+guess = 0  # the guess starts as any number != n
+count = 0  # this is the guess count, to be printed at the end
 while int(guess) != n:
-    guess = input("Guess the number from 1 through 9!\n")
-    if "exit" in guess:
+    guess = input("Guess the number from 1 through 9!! Or type exit \n")  # stores the user guess
+    if "exit" in guess:  # if the user wants to quit
         break
-    elif int(guess) > n:
+    elif int(guess) > n:  # if guess is higher than the number
         print("Too High!")
-    elif int(guess) < n:
+    elif int(guess) < n:  # if guess is lower than the number
         print("Too Low!")
 
-    count += 1
-if guess != str(n):
+    count += 1  # after the guess, count is increased by one
+if guess != str(n):  # if this happens, the user exited the loop without guessing it right
     beg = input("Quitter! I'll never tell you which number it was! You only tried " + str(count) + " times!\n ")
-    if "please" or "pls" in beg:
+    if "please" or "pls" in beg:  # easter egg
         print("Ok it was " + str(n) + " ...")
 else:
     print("Good job! the random number was " + str(n) + " and you got it in " + str(count) + " guesses!")
